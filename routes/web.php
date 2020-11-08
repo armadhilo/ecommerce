028597d7edffcdde6264c05e2d4bb97821ca9ce4
armadhilo <dunsforce@gmail.com>
2020-11-08 15:41:03 +0700

    first

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,6 @@
 |
 */
 
-Route::get('/','Product@index');
-
-Route::get('/category','Category@index');
-Route::post('/save_category','Category@save_category');
-Route::get('/ajax_list','Category@ajax_list');
-Route::get('/edit/{id}', 'Category@edit');
-Route::post('/update_category', 'Category@update_category');
-Route::get('/delete/{id}', 'Category@hapus');
+Route::get('/', function () {
+    return view('welcome');
+});
