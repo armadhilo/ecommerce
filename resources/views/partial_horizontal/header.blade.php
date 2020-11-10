612b4aba5295559e6bfafcc4f9fc07b1320e5d35
Dinda Anggraini <dinda.anggraini27@gmail.com>
2020-11-10 18:07:28 +0700

    update filter

--- resources/views/partial_horizontal/header.blade.php
+++ resources/views/partial_horizontal/header.blade.php
@@ -12,13 +12,6 @@
                     </ul>
                 </div>
                 <ul class="nav navbar-nav float-right">
-                    
-                    
-                    <li class="dropdown dropdown-user nav-item">
-                        <a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
-                            <div class="user-nav d-sm-flex d-none"><span class="user-name text-bold-600">John Doe</span><span class="user-status">Available</span></div><span><img class="round" src="../../../app-assets/images/portrait/small/avatar-s-11.jpg" alt="avatar" height="40" width="40"></span>
-                        </a>
-                    </li>
                 </ul>
             </div>
         </div>
@@ -32,26 +25,38 @@
     <div class="header-navbar navbar-expand-sm navbar navbar-horizontal floating-nav navbar-light navbar-without-dd-arrow navbar-shadow menu-border" role="navigation" data-menu="menu-wrapper">
         <div class="navbar-header">
             <ul class="nav navbar-nav flex-row">
-                <li class="nav-item mr-auto"><a class="navbar-brand" href="../../../html/ltr/horizontal-menu-template/index.html">
-                        <div class="brand-logo"></div>
-                        <h2 class="brand-text mb-0">Vuexy</h2>
+                <li class="nav-item mr-auto"><a class="navbar-brand" href="javascript:void(0)">
+                        <img src="{{ asset('app-assets/images/logo/logo-unesa.png') }}" style="width: 36px;">
+                        <h2 class="brand-text mb-0">Inovasi Unesa</h2>
                     </a></li>
                 <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i><i class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block collapse-toggle-icon primary" data-ticon="icon-disc"></i></a></li>
             </ul>
         </div>
         <!-- Horizontal menu content-->
         <div class="navbar-container main-menu-content" data-menu="menu-container">
-            <!-- include ../../../includes/mixins-->
             <ul class="nav navbar-nav" id="main-menu-navigation" data-menu="menu-navigation">
-                <li class="dropdown nav-item active" data-menu="dropdown"><a class="nav-link" href="#"><i class="feather icon-home"></i><span data-i18n="Home">Home</span></a>
+                <li class="dropdown nav-item active"><a class="nav-link" href="#"><i class="feather icon-home"></i><span data-i18n="Home">Home</span></a>
                  </li>
-                 <li class="dropdown nav-item" data-menu="dropdown"><a class="nav-link" href="#"><i class="feather icon-package"></i><span data-i18n="Product">Product</span></a>
+                 <li class="dropdown nav-item"><a class="nav-link" href="#"><i class="feather icon-package"></i><span data-i18n="Product">Product</span></a>
                  </li>
-                 <li class="dropdown nav-item" data-menu="dropdown"><a class="nav-link" href="#"><i class="fa fa-building-o"></i><span data-i18n="AboutUs">About Us</span></a>
+                 <li class="dropdown nav-item"><a class="nav-link" href="#"><i class="fa fa-building-o"></i><span data-i18n="AboutUs">About Us</span></a>
                  </li>
-                 <li class="dropdown nav-item" data-menu="dropdown"><a class="nav-link" href="#"><i class="feather icon-phone"></i><span data-i18n="ContactUs">Contact Us</span></a>
+                 <li class="dropdown nav-item"><a class="nav-link" href="#"><i class="feather icon-phone"></i><span data-i18n="ContactUs">Contact Us</span></a>
                  </li>
             </ul>
+        </div>
+
+        <div class="nav navbar-nav float-right" style="width: 300px;" id="nav_logo">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-4 text-right">
+                        <img src="{{ asset('app-assets/images/logo/logo-unesa.png') }}" style="width: 36px;">
+                    </div>
+                    <div class="col-md-8 p-0">
+                        <h4 class="brand-text mb-0" style="color: #3f9ce8; font-weight: 600; padding-top: 8px;">Inovasi Unesa</h4>    
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
