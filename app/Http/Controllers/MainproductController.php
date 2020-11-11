d8c5990f698a7d1dca7772168679055ac2792f59
armadhilo <dunsforce@gmail.com>
2020-11-11 11:39:58 +0700

    hello

--- app/Http/Controllers/MainproductController.php
+++ app/Http/Controllers/MainproductController.php
@@ -8,13 +8,18 @@ use DB;
 class MainproductController extends Controller
 {
     public function index(Request $request){
+        
         $search = $request->search_product;
-        $filter = $request->filter;
-        $data['slider'] = DB::table('slider')->limit(3)->get();
+        $filter = $request->category;
+        
+        $data['slider'] = DB::table('slider')->whereNull('deleted_at')->limit(3)->get();
+        $data['category'] = DB::table('category')->get();
+        
         $data['product'] = DB::table('product')->where([
             ['product_name', 'like', '%'.$search.'%'],
             ['category_id', 'like', '%'.$filter.'%']
-        ])->paginate(1);
+        ])->whereNull('deleted_at')->paginate(1);
+        
         return view('main_product.main_product',$data);
     }
     
