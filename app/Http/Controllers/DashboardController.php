9f9173dfbed340f71b40ef40b88356717109d874
armadhilo <dunsforce@gmail.com>
2020-11-12 17:09:20 +0700

    hel

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,13 +19,13 @@ class DashboardController extends Controller
         $label_product = [];
         $data_product = [];
         
-        $query = DB::table('product')->join('category', 'product.category_id', '=', 'category.id')->select('count','product_name')->orderByDesc('count')->limit(10)->get();
+        $query = DB::table('product')->join('category', 'product.category_id', '=', 'category.id')->select('count','product_name')->whereNull('deleted_at')->orderByDesc('count')->limit(10)->get();
         foreach($query as $item){
             array_push($label_mostClick,$item->product_name);
             array_push($data_mostClick,$item->count);
         }
 
-        $query2 = DB::select( DB::raw("SELECT COUNT(category_id) AS hitung, category_name FROM product as a JOIN category as b ON a.category_id = b.id GROUP BY category_id ORDER BY hitung DESC"));
+        $query2 = DB::select( DB::raw("SELECT COUNT(category_id) AS hitung, category_name FROM product as a JOIN category as b ON a.category_id = b.id WHERE deleted_at IS NULL GROUP BY category_id ORDER BY hitung DESC"));
         foreach($query2 as $item){
             array_push($label_product,$item->category_name);
             array_push($data_product,$item->hitung);
--- resources/views/partial_horizontal/footer.blade.php
+++ resources/views/partial_horizontal/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="footer footer-static footer-light navbar-shadow">
     <p class="clearfix blue-grey lighten-2 mb-0">
         <span class="float-md-right d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2020
-            <a class="text-bold-800 grey darken-2" href="javascript:void(0)" target="_blank">Unesa,</a>All rights Reserved</span>
+            <a class="text-bold-800 grey darken-2" href="javascript:void(0)" target="_blank">LPPM Unesa,</a>All rights Reserved</span>
             
     </p>
 </footer>
